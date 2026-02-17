3c5ae2c258be8fc7159e7614c34657a6b311d6a2
sounya94 <sounya-abattouy@live.be>
2026-02-17 11:06:39 +0100

    commit

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -1,8 +1,31 @@
-void main() {
+/*void main() {
 IO.println("Hello World");
-IO.readln("geef iets in: ");
-}
+String ingegeven = IO.readln("geef iets in: ");
+int getal = Integer.parseInt(ingegeven);
+IO.println("het is ingegeven " + getal*5);
+}*/
 
 /*public static void main (String[] args){
     System.out.println("heeyy");
 }*/
+
+/*int functie (int a, int b){
+    //IO.println(a+b);
+    return a+b;
+}
+
+void main () {
+    //functie (3,7);
+    IO.println(functie(3,7));
+}*/
+
+void main(){
+    //int a = 5;
+    //float b = 5;
+    //IO.println(b/2);
+    //float b = 5/2;  // int delen door int je plaatst dat in een float dus je bent die komma al kwijt vooraleer je het in een float plaatst.
+    //IO.println(b);
+    float c = 5f/2; //je maakt de 5 een float ervan
+    IO.println(c);
+
+}
