c16e6358ec916bbf08057c5c2e63cb8837d20560
sounya94 <sounya-abattouy@live.be>
2026-02-17 09:44:17 +0100

    commit

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -1,7 +1,8 @@
 void main() {
 IO.println("Hello World");
+IO.readln("geef iets in: ");
 }
 
-public static void main (String[] args){
+/*public static void main (String[] args){
     System.out.println("heeyy");
-}
+}*/
