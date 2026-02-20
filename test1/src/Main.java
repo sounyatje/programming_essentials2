9985c3f8cd8745d1c2afa109709a18ead2fb2c46
sounya94 <sounya-abattouy@live.be>
2026-02-20 14:02:48 +0100

    commit

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -19,7 +19,7 @@ void main () {
     IO.println(functie(3,7));
 }*/
 
-void main(){
+/*void main(){
     //int a = 5;
     //float b = 5;
     //IO.println(b/2);
@@ -28,4 +28,50 @@ void main(){
     float c = 5f/2; //je maakt de 5 een float ervan
     IO.println(c);
 
+}*/
+
+/*void main (){
+    boolean test = true;
+    char t = 'R';
+    //IO.println(t+1);
+    IO.println((char)(t+1));
+}*/
+
+/*void main(){
+    for (int i = 0; i<26; i++) {
+        char t = (char) ('a' + i);
+        IO.println(t);
+    }
+
+    int a = 5;
+    while(a<8){
+        a++;
+        IO.println("uitvoer: " + a);
+    }
+
+    do{
+        a--;
+        IO.println("uitvoer: " + a);
+    } while(a>3);
+
+    switch (a) {
+        case1:
+        IO.println();
+        break;
+        case2:
+        IO.println();
+        break;
+        case3:
+        IO.println("gelukt");
+        break;
+        default: IO.println("iets anders");
+    }
+}*/
+
+void main () {
+
+    String[] studenten = new String[10];
+    for(int i = 0; i< studenten.length; i++){
+        IO.println(studenten[i]);
+    }
 }
